dc443c1ba5cee9370aa289206377e5c39be91eed
SaeedJN <saeedi1372@yahoo.com>
2025-08-29 21:43:52 +0330

    ex1-2 Getting input from the user and print them

---
+++ ex1-2.py
@@ -0,0 +1,9 @@
+number1 = int(input("please enter a number1 : "))
+number2 = int(input("please enter a number2 : "))
+
+total = number1 + number2
+sub = number1 - number2
+mul = number1 * number2
+div = number1 / number2
+
+print(f"sum: {total}, sub: {sub}, mul: {mul}, div: {div}")
