e3b0e337c65e3a2445f820ff61c0141bae385ef3
SaeedJN <saeedi1372@yahoo.com>
2025-08-30 14:10:31 +0330

    import math functions

---
+++ ex10-1.py
@@ -0,0 +1,14 @@
+from math import sqrt, pi
+def welcomeUSER(msg):
+    print(f"Hello {msg}!")
+
+inputName = input("enter your Name: ")
+
+welcomeUSER(inputName)
+
+inputNumber = int(input("enter your number: "))
+
+
+print(sqrt(inputNumber))
+print(pi)
+
