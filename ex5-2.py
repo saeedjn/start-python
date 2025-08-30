ef176903fc54008f0bea9266a376c9519a68c5dd
SaeedJN <saeedi1372@yahoo.com>
2025-08-30 13:52:27 +0330

    Getting input from the user and print length,strip,split,upper,word[index]

---
+++ ex5-2.py
@@ -0,0 +1,15 @@
+input_sen = input("enter your Sentences: ")
+
+sn = input_sen.strip()
+
+words = input_sen.split()
+
+
+print(len(sn))
+
+print(input_sen.upper())
+
+
+print(words[0], words[-1])
+
+
