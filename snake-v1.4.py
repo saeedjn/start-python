46e7c14b44a0b9570211361d8b84758ee9763f22
SaeedJN <saeedi1372@yahoo.com>
2025-08-30 14:24:15 +0330

    build snake project version-1.4 import colorama for color and position for print score

---
+++ snake-v1.4.py
@@ -0,0 +1,70 @@
+import random
+import os
+import time
+import msvcrt
+from colorama import init, Cursor
+
+init()
+
+rows = 15
+cols = 35
+
+board = [[" " for _ in range(cols)] for _ in range(rows)]
+snake = [(7,7),(7,6),(7,5)]
+food = (random.randint(0,rows-3), random.randint(0,cols-3))
+direction = "d"
+score = 0
+
+
+def draw_board(snake,food) :
+    os.system("cls")
+    print(Cursor.POS(0,0) + f"your Score: {score}")
+    for r in range(rows) :
+        row_str = ""
+        for c in range(cols) :
+            if (r - 1,c) in snake:
+                row_str += "o"
+            elif (r - 1,c) == food : 
+                row_str += "*"
+            elif r == 0 or r == rows - 1 or c == 0 or c == cols - 1 : 
+                row_str += "#"
+            else:
+                row_str += " "
+        print(Cursor.POS(0,r+2) + row_str)
+
+def get_key():
+    if msvcrt.kbhit():
+        key = msvcrt.getch().decode("utf-8").lower()
+        if key in ["w", "s", "a", "d"]:
+            return key
+    return None
+
+while True :
+    draw_board(snake,food)
+    key = get_key()
+    if key:
+        direction = key
+    head_row, head_col = snake[0]
+    if direction == "w" : 
+        new_head = ((head_row - 1) % rows, head_col)
+    elif direction == "s": 
+        new_head = ((head_row + 1) % rows, head_col)
+    elif direction == "d" :
+        new_head = (head_row, (head_col + 1) % cols)
+    elif direction == "a" :
+        new_head = (head_row, (head_col - 1) % cols)
+    
+    if new_head in snake:
+        print("Game Over")
+        print(f"your Score: {score}")
+        break
+    snake.insert(0, new_head)
+    if new_head != food :
+        snake.pop()
+    else :
+        while True:
+            score += 1                 
+            food = (random.randint(0,rows-2), random.randint(0,cols-2))
+            if food not in snake:
+                break
+    time.sleep(0.5)       
